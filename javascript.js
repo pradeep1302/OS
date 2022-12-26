fea35a8e5a801c2d958298b49a0ba4ece8a31dc9
pradeep1302 <112539810+pradeep1302@users.noreply.github.com>
2022-12-26 09:18:00 +0530

    Update javaScript

--- javascript.js
+++ javascript.js
@@ -1,16 +1,5 @@
 function roll() {
-    var number = Math.round((Math.random() * 5) + 1);
+    var number = Math.floor(Math.random() * 6) + 1;
     document.querySelector("h1").innerHTML = number;
-    if (number === 1)
-        document.querySelector("div").innerHTML = '<img src="./1.jpg">';
-    else if (number === 2)
-        document.querySelector("div").innerHTML = '<img src="./2.jpg">';
-    else if (number === 3)
-        document.querySelector("div").innerHTML = '<img src="./3.jpg">';
-    else if (number === 4)
-        document.querySelector("div").innerHTML = '<img src="./4.jpg">';
-    else if (number === 5)
-        document.querySelector("div").innerHTML = '<img src="./5.jpg">';
-    else if (number === 6)
-        document.querySelector("div").innerHTML = '<img src="./6.jpg">';
+    document.querySelector("div").innerHTML = "<img src=./" + number + ".jpg>";
 }
